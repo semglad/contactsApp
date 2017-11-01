530d328544b74f6b3dd628aa89e9523b3fc71d1e
Sem Glad <sem.glad@student.saimia.fi>
2017-11-01 09:28:14 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/contact/contacts.ts
@@ -0,0 +1,17 @@
+export class Contact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  streetAddress: string;
+  city: string;
+
+  constructor (id: number, firstName: string, lastName: string, phone: string, streetAddress: string, city: string) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.streetAddress = streetAddress;
+    this.city = city;
+  }
+}
